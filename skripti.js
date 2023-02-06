8fed057c3b615a82d5c731c09192e9fde0081971
strelnieksj <122868522+strelnieksj@users.noreply.github.com>
2023-02-06 15:01:27 +0200

    Create skripti.js

---
+++ skripti.js
@@ -0,0 +1,11 @@
+//alert("Čau!")
+console.log("Kas i, ko?");
+
+var logs = document.querySelector(".chataZinas");
+var vards = document.querySelector("#vards");
+var zina = document.querySelector("#zina");
+
+function sutitZinu(){
+    console.log("Poga darbojas");
+    console.log("ziņu ir: "+zina.value);
+}
